c056badac0b53ab6bdb4f74d696f3c6e2c23dc81
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-16 18:24:19 +0100

    delete watchNode2.js -- no longer necessary

--- scripts/watchNode.sh
+++ scripts/watchNode.sh
@@ -2,5 +2,4 @@
 sh scripts/linkLocalDeps.sh
 deno bundle --watch bundlable/deps.b.js node/bundlable/deps.b.js &
 deno run -A --watch scripts/watchNode.js &
-deno run -A --watch scripts/watchNode2.js &
 esbuild node/portable/main.js --outfile=node/bundle.js --bundle --watch --platform=node
